42ef4481812c5b8eb0924f94e76dfaba0df5fa73
poyraz76 <ergunsalman@hotmail.com>
2017-02-22 16:19:53 +0300

    chmod

--- compact-lib32bit/actions.py
+++ compact-lib32bit/actions.py
@@ -5,8 +5,11 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
 WorkDir = "."
 
 def install():
     pisitools.insinto("usr/lib32/", "./lib32/*")
+    shelltools.system("chmod -R 755 %s/usr/lib32/* " %get.installDIR())
