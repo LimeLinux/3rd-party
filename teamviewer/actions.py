73ef2031a566155db94e86a49b385b1e05ed64bf
poyraz76 <ergunsalman@hotmail.com>
2018-02-13 19:18:22 +0300

    teamviewer x86_64 update

--- teamviewer/actions.py
+++ teamviewer/actions.py
@@ -11,24 +11,23 @@ from pisi.actionsapi import get
 WorkDir = "."
 NoStrip = ["/opt/teamviewer/tv_bin/wine/drive_c/TeamViewer/tvwine.dll.so"]
 
-def setup():
-    shelltools.system("rpm2targz -v %s/teamviewer_12*.i686.rpm" %get.workDIR())
-    shelltools.system("tar xfvz %s/teamviewer_12*.i686.tar.gz --exclude=var --exclude=etc --exclude=usr" %get.workDIR())
     
 def install():
-    pisitools.insinto("/opt/", "./opt/*")
-    pisitools.remove("/opt/teamviewer/tv_bin/xdg-utils/xdg-email")
+    pisitools.insinto("/", "./opt")
+    pisitools.insinto("/", "./etc")
+    pisitools.insinto("/", "./usr")
+    pisitools.insinto("/", "./var")
+
     
     #necessary symlinks
     pisitools.dosym("/opt/teamviewer/tv_bin/teamviewerd", "etc/init.d/teamviewerd")
-    pisitools.dosym("/opt/teamviewer/tv_bin/script/teamviewer", "usr/bin/teamviewer")
     pisitools.dosym("/opt/teamviewer/logfiles", "var/log/teamviewer")
-    pisitools.dosym("/opt/teamviewer/config", "etc/teamviewer")
+
     
     pisitools.dodoc("%s/opt/teamviewer/doc/License.txt" %get.workDIR())
     
     
-    shelltools.chmod("%s/opt/teamviewer/*" % get.installDIR(),0755)  
+ 
     
     
     
